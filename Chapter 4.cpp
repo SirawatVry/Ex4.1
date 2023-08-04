8e9ca750a26fa661a5007ad09c4a784c86aa4b8a
SirawatVry <139094315+SirawatVry@users.noreply.github.com>
2023-08-04 15:25:22 +0700

    Add files via upload

---
+++ Chapter 4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+int Age,bmi;
+float H,W;
+string Gen;
+cout << "Enter Age  :";
+cin  >> Age;
+cout << "Enter Gender Male\\Female:";
+cin  >> Gen;
+cout << "Enter Height  :";
+cin  >> H;
+cout << "Enter Weight  :";
+cin  >> W;
+bmi = W / (H/100*H/100);
+cout << "BMI = "<< bmi<<endl;
+if (bmi<18.5) cout << "Underweight\n";
+else if (bmi<25) cout << "Normal\n";
+else if (bmi<30) cout << "Overweight\n";
+else if (bmi<40) cout << "Overweight\n";
+return(0);
+}
